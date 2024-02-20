14ee1a2bcd6060e64e8f88ab08a4003398e41eaa
programflow <kevinfloort@gmail.com>
2024-02-20 12:47:19 -0500

    Modified the __main__.py file to be a bit more modular and added a replay feature

--- higher_lower/__main__.py
+++ higher_lower/__main__.py
@@ -2,28 +2,47 @@ import random
 from game_data import data
 import art
 
-
-score = 0
-continue_game = True
-while continue_game == True:
+def generate_choices():
 	item_A = data[random.randint(0, len(data)-1)]
 	item_B = data[random.randint(0, len(data)-1)]
 	print(art.logo)
 	print(f"Compare A: {item_A['name']}, a {item_A['description']}, from {item_A['country']}")
 	print(art.vs)
 	print(f"Against B: {item_B['name']}, a {item_B['description']}, from {item_B['country']}")
-	choice = input("Who has more followers? Type 'A' or 'B': ")
-
-	if item_A['follower_count'] > item_B['follower_count']:
-		correct_answer = "A"
-	else:
-		correct_answer = "B"
-
-	if choice == correct_answer:
-		score += 1
-		print(f"You're right! Current score: {score}")
-	else:
-		print(f"Sorry, That's wrong. Final score: {score}")
-		continue_game = False
+	
+	return item_A, item_B
 
 
+
+def play():
+	score = 0
+	continue_game = True
+
+	
+	
+
+	while continue_game == True:
+		item_A, item_B = generate_choices()
+		choice = input("Who has more followers? Type 'A' or 'B': ")
+		
+
+		if item_A['follower_count'] > item_B['follower_count']:
+			correct_answer = "A"
+		else:
+			correct_answer = "B"
+
+		if choice == correct_answer:
+			score += 1
+			print(f"You're right! Current score: {score}")
+		else:
+			print(f"Sorry, That's wrong. Final score: {score}")
+			continue_game = False
+
+start = 'y'
+while start == 'y':
+
+
+
+	play()
+
+	start = input("Would you like to play again?(y/n) ")
